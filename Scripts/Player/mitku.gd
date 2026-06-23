e3c7fa6c28ad5cbedcfdf39c0facefe2add9c327
Hamza Latif <hamzalatif@Mac.lan>
2026-06-23 16:01:58 +0100

    Replicated strangeness of the Undertale walking animation behaviour.

--- Scripts/Player/mitku.gd
+++ Scripts/Player/mitku.gd
@@ -5,8 +5,10 @@ extends CharacterBody2D
 @export var speed = 80
 
 var direction: Vector2
+var unscaled_direction: Vector2
 var last_direct = "down"
-var last_direct_vector: Vector2
+var last_direct_vector: Vector2 = Vector2(0, 1)
+var locked: bool = false
 
 func get_input():
 	direction = Input.get_vector("left", "right", "up", "down")
@@ -20,8 +22,7 @@ func get_input():
 		else:
 			direction[dr] = 0
 	
-	if direction:
-		last_direct_vector = direction
+	unscaled_direction = direction
 	
 	# Scale the diagonal speed down a bit
 	# Not fully normalised because that's boring
@@ -30,34 +31,58 @@ func get_input():
 	
 	velocity = direction * speed
 	
-	# Animation stuff
+func animate():
 	
 	#if direction:
 		#if direction.dot(last_direct_vector) <= 0:
+	
+	if unscaled_direction:
+		if unscaled_direction != last_direct_vector:
+			pass
+	
+	if unscaled_direction:
+		if abs(unscaled_direction.x) != abs(unscaled_direction.y):
+			locked = true
+			if direction.x < 0:
+				animation.play("walk_side")
+				animation.flip_h = true
+				# This is unbelievably bad
+				last_direct = "left"
+			elif direction.x > 0:
+				animation.play("walk_side")
+				animation.flip_h = false
+				last_direct = "right"
+			elif direction.y > 0:
+				animation.flip_h = false
+				animation.play("walk_down")
+				last_direct = "down"
+			elif direction.y < 0:
+				animation.flip_h = false
+				animation.play("walk_up")
+				last_direct = "up"
+		elif not locked:
+			print(unscaled_direction)
+			if direction.x < 0:
+				animation.play("walk_side")
+				animation.flip_h = true
+				# This is unbelievably bad
+				last_direct = "left"
+			elif direction.x > 0:
+				animation.play("walk_side")
+				animation.flip_h = false
+				last_direct = "right"
+			elif direction.y > 0:
+				animation.flip_h = false
+				animation.play("walk_down")
+				last_direct = "down"
+			elif direction.y < 0:
+				animation.flip_h = false
+				animation.play("walk_up")
+				last_direct = "up"
+			locked = true
 			
-	
-	# Flip sidewalk
-	if direction.x < 0:
-		animation.play("walk_side")
-		animation.flip_h = true
-		# This is unbelievably bad
-		last_direct = "left"
-	elif direction.x > 0:
-		animation.play("walk_side")
-		animation.flip_h = false
-		last_direct = "right"
-	
-	if not direction.x:
-		if direction.y > 0:
-			animation.flip_h = false
-			animation.play("walk_down")
-			last_direct = "down"
-		elif direction.y < 0:
-			animation.flip_h = false
-			animation.play("walk_up")
-			last_direct = "up"
-		
-	if not direction:
+	if not unscaled_direction:
+		locked = false
 		match last_direct:
 			"left":
 				animation.play("idle_side")
@@ -72,6 +97,9 @@ func get_input():
 				animation.play("idle_up")
 				animation.flip_h = false
 	
+	last_direct_vector = unscaled_direction
+	
 func _physics_process(delta: float) -> void:
 	get_input()
 	move_and_slide()
+	animate()
